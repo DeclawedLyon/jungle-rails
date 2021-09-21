7edce212cec55f31ea486770b7f4c90849632f88
DeclawedLyon <declan.e.lyons@gmail.com>
2021-09-20 20:50:33 -0600

    Created category controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,12 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
+    resources :categories, only: [:index, :show, :new]
     resources :products, except: [:edit, :update, :show]
   end
 
+  # resources :categories, except: [:destroy, :edit, :update, :show]
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
