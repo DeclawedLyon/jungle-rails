38ebd3bec466af3496abbc8a5dc9ddf64a31bdd2
DeclawedLyon <declan.e.lyons@gmail.com>
2021-09-20 23:04:40 -0600

    added featue - users can now login and logout of app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-
+  
   root to: 'products#index'
+  
+
+  # These routes will be for signup. The first renders a form in the browse, the second will 
+  # receive the form and create a user in our database using the data given to us by the user.
+  
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
   
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
@@ -18,6 +30,7 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :show, :new]
     resources :products, except: [:edit, :update, :show]
   end
+    
 
   # resources :categories, except: [:destroy, :edit, :update, :show]
   
