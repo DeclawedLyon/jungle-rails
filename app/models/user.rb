c4654e5a728148459f26e8c6107e782b415b5e16
DeclawedLyon <declan.e.lyons@gmail.com>
2021-09-23 20:37:21 -0600

    adds product tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
-
   has_secure_password
-
+  
   def authenticate_with_credentials(email, password)
-    
+
   end
 
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,44 +3,40 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
-    it 'saves successfully when user provides all required fields' do
-      @category = Category.new(name: 'test category')      
-      @product = @category.products.new(name: 'test product', price: 40, quantity: 3)
-      @product.save!
-    end
+
     # when name is not provided
     it 'returns an error when the name field is not provided' do
-      @category = Category.new(name: 'test category')
-      @product = @category.products.new( price: 40, quantity: 3)
-      @product.save
-      puts @product.errors.full_messages
-      expect(@product.errors.full_messages).to include("Name can't be blank")
+      @category = Category.new(name: 'test category')      
+      @product = @category.products.new(name: nill, price: 40, quantity: 3, category: @category)
+      expect(@product.errors.full_messages).to include ("Name can't be blank")    
     end
-
+    
     # when price is not provided
     it 'returns an error when the price field is not provided' do
-      @category = Category.new(name: 'test category')
-      @product = @category.products.new(name: 'test product', quantity: 3)
-      @product.save
-      puts @product.errors.full_messages
-      expect(@product.errors.full_messages).to include("Price can't be blank")
+      @category = Category.new(name: 'test category')      
+      @product = @category.products.new(name: 'test product', price: nill, quantity: 3, category: @category)
+      expect(@product.errors.full_messages).to include ("Price can't be blank")
     end
 
     # when quantity is not provided
     it 'returns an error when the quantity field is not provided' do
       @category = Category.new(name: 'test category')
-      @product = @category.products.new(name: 'test product', price: 40)
-      @product.save
-      puts @product.errors.full_messages
-      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+      @product = @category.products.new(name: 'test product', price: 40, quantity: nill, category: @category)
+      expect(@product.errors.full_messages).to include ("Quantity can't be blank")
     end
 
     # when category is not provided
     it 'returns an error when the category field is not provided' do
-      @product = Product.new(name: 'test product', price: 40, quantity: 3)
-      @product.save
-      puts @product.errors.full_messages
-      expect(@product.errors.full_messages).to include("Category can't be blank")
+      @category = Category.new(name: 'test category')      
+      @product = Product.new(name: 'test product', price: 40, quantity: 3, category: nill)
+      expect(@product.errors.full_messages).to include ("Category can't be blank")
+    end
+
+    # when all fields are provided
+    it 'should create new product with no error' do
+      @category = Category.new(name: "test category")
+      @product = Product.create(name: "test product", price: 40, quantity: 3, category:@category)
+      expect(@product).to be_valid
     end
 
   end
